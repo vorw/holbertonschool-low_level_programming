0217504b646373028de44d613e0cf4b1dbc0be06
vorw <sultanalsanie23@gmail.com>
2025-05-01 15:23:38 +0000

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int number;
+
+	for (number = 48; number <= 57; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
