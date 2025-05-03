40115f048d1b8088d4063e9d79f1a382c332cafd
vorw <sultanalsanie23@gmail.com>
2025-05-03 20:15:40 +0000

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints alphabet ten times!
+*
+* Description: please betty..
+*/
+
+void print_alphabet_x10(void)
+{
+	int x;
+	char c;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+	}
+}
