f47c37712762e829f457d8fc12280c9dd557a34d
vorw <sultanalsanie23@gmail.com>
2025-05-03 19:52:50 +0000

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
