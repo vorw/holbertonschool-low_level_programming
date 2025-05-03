28cb932df1e536f03d3bb518fea7fc8a531dd64c
vorw <sultanalsanie23@gmail.com>
2025-05-03 20:32:39 +0000

    task 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* _islower - wasn't named cause of the definition of slow.. it just checks if its a small letter
-*
-* Description: betty style work again, please
+* _islower - just checks if its a small letter, its not slow
+* @c: a ASCII value checker
+* Return: returns 1 if c is lowercase, returns 0 otherwise
 */
 
 int _islower(int c)
