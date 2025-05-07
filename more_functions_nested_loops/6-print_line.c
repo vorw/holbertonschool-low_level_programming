61308a5aaa7bdca8283603e6aeed9b01fa580c28
vorw <sultanalsanie23@gmail.com>
2025-05-07 15:18:48 +0000

    Task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - writes a function that draws a straight line in the terminal
+ * @n: the number of times the character _ should be printed
+ */
+
+void print_line(int n)
+{
+	if ( n > 0)
+	{
+		_putchar('_');
+		_putchar('\n');
+	}
+
+	else
+	{
+		_putchar('\n');
+	}
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -19,5 +19,6 @@ void print_to_98(int n);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
 
 #endif
