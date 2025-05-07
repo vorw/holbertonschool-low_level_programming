d9991dd5cf0a9bfec667aac30ddc0e1da6395136
vorw <sultanalsanie23@gmail.com>
2025-05-07 18:13:20 +0000

    task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,23 +13,24 @@ int main(void)
 	{
 		if (x % 3 == 0 && x % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (x % 3 == 0 && x % 5 != 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 
 		}
 		else if (x % 5 == 0 && x % 3 != 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			if (x != 100)
-			{
-				printf("%i ", x);
-			}
+			printf("%i", x);
+		}
+		if (x != 100)
+		{
+			printf(" ");
 		}
 	}
 
