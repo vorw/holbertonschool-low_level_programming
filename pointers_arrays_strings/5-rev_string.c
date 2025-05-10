1da14951d8eebcc5fcda470d98c48e70721f3c02
vorw <sultanalsanie23@gmail.com>
2025-05-10 21:23:21 +0000

    task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,29 +1,29 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* rev_string - reverses a string.
+* rev_string - reverses a string
 * @s: string
 */
 
 void rev_string(char *s)
 {
-    int a = 0;
-    int b = 0;
-    char tmp;
-    
-    while (s[a] != '\0')
-    {
-	    a++;
-    }
-    
-    a--;
-    
-    while (b < a)
-    {
-	    tmp = s[b];
-	    s[b] = s[a];
-	    s[a] = tmp;
-	    b++;
-	    a--;
-    }
+	int a = 0;
+	int b = 0;
+	char tmp;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	a--;
+
+	while (b < a)
+	{
+		tmp = s[b];
+		s[b] = s[a];
+		s[a] = tmp;
+		b++;
+		a--;
+	}
 }
