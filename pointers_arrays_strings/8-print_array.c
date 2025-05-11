7dea7a99edc34db3b0280ce22effc1206f16535d
vorw <sultanalsanie23@gmail.com>
2025-05-11 07:14:19 +0000

    Task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_array - prints n elements of an array of integers
+* @a: pointer
+* @n: number of elements
+*/
+
+void print_array(int *a, int n)
+{
+	int z;
+
+	for (z = 0; z < n; z++)
+	{
+		printf("%i", a[z]);
+		
+		if (z < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	
+	printf("\n");
+}
