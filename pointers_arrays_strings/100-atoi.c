f4e957503c6e4d79f2b95128629a0cca2f149224
vorw <sultanalsanie23@gmail.com>
2025-05-11 07:50:07 +0000

    Task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,26 +11,28 @@ int _atoi(char *s)
 	int a = 0;
 	int number = 0;
 	int sign = 1;
+	int detect = 0;
 
-	while (s[a] == ' ')
+	while (s[a] != '\0')
 	{
-		a++;
-	}
-
-	if (s[a] == '-' || s[a] == '+')
-	{
-		if (s[a] == '-')
+		if (s[a] == '-' && !detect)
 		{
-			sign = -1;
-			a++;
+			sign *= -1;
 		}
-	}
+		
+		else if (s[a] >= '0' && s[a] <= '9')
+		{
+			number = number * 10 + (s[a] - '0');
+			detect = 1;
+		}
 
-	while (s[a] >= '0' && s[a] <= '9')
-	{
-		number = number * 10 + (s[a] - '0');
+		else if (detect)
+		{
+			break;
+		}
+
 		a++;
 	}
-
+	
 	return (number * sign);
 }
