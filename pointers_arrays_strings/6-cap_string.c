c3c97de7191af6db013a6d1766db162d44486346
vorw <sultanalsanie23@gmail.com>
2025-05-11 09:16:29 +0000

    Task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* cap_string - capitalizes all words of string
+* @s: string
+* Return: string
+*/
+
+char *cap_string(char *s)
+{
+	char x[] = " \t\n,;.!?\"(){}";
+	int a = 0;
+	int b;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+	{
+		s[0] -= 32;
+	}
+
+	while (s[a])
+	{
+		for (b = 0; x[b]; b++)
+		{
+			if (s[a] == x[b] && s[a + 1] >= 'a' && s[a + 1] <= 'z')
+			{
+				s[a + 1] -= 32;
+			}
+
+			a++;
+		}
+	}
+
+	return (s);
+}
