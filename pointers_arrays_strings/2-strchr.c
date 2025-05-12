d2b902e670d456bcd7200223620fb1dc68c81a8f
vorw <sultanalsanie23@gmail.com>
2025-05-12 08:53:34 +0000

    task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates char in string
+ * @s: pointer to string
+ * @c: char to locate
+ * Return: pointer to the first occurrence of c in s, or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+
+		s++;
+	}
+
+	return (NULL);
+}
