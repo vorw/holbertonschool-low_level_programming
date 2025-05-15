16c170cfe66d782a4ad1052522b4f96d21355e0b
vorw <sultanalsanie23@gmail.com>
2025-05-15 13:08:51 +0000

    task 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: string
+ *
+ * Return: betty style
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
