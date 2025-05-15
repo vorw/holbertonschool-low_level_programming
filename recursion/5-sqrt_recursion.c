b452453cdc97c98a2aa4f873f5ee150fed295869
vorw <sultanalsanie23@gmail.com>
2025-05-15 13:44:01 +0000

    Task 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * find_sqrt - helps
+ * _sqrt_recursion - returns n.s.r of number
+ * @n: number 1
+ * @m: number 2
+ * Return: natural square root, otherwise -1 if its not a n.s.r
+ */
+
+int find_sqrt(int n, int m)
+{
+	if (m * m == n)
+	{
+		return (m);
+	}
+
+	if (m * m > n)
+	{
+		return (-1);
+	}
+
+	return (find_sqrt(n, m + 1));
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	return (find_sqrt(n, 0));
+}
