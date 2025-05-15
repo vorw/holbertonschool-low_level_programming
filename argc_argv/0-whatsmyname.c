7f1c7f220d397e2ec1967648f59dea1e9132ece4
vorw <sultanalsanie23@gmail.com>
2025-05-15 15:01:12 +0000

    Task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main
+ * @argc: argc
+ * @argv: argv
+ * Return: betty
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
