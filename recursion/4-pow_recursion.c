42643569520b28572418ebe6d9595ec424be2461
vorw <sultanalsanie23@gmail.com>
2025-05-15 13:28:06 +0000

    task 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the power of two nums
+ * @x: first num
+ * @y: second num
+ * Return: the power of two nums, otherwise -1
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 1)
+	{
+		return (-1);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
