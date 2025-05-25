ba9f3b3608b252e85fa50fc3854c7e06aef879e0
vorw <sultanalsanie23@gmail.com>
2025-05-25 16:16:28 +0000

    Task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dog info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
