e57ccf586eceee7017ae58325a8b78819d18f834
vorw <sultanalsanie23@gmail.com>
2025-06-15 16:10:34 +0000

    Task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: first element of list
+ * Return: Number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h->next;
+	}
+
+	return (count);
+}
