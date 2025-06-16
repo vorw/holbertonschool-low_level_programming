790e086a1008763cdd434b6b28c393e9a91ed669
vorw <sultanalsanie23@gmail.com>
2025-06-16 14:07:54 +0000

    Task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,6 +12,7 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	unsigned int len = 0;
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
@@ -26,7 +27,11 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	while (str[len])
+	{
+		len++;
+	}
+
 	new_node->next = *head;
 	*head = new_node;
 
