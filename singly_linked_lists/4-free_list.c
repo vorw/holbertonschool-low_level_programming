c9cbc8f69742404d3eb6e5cd0e1437cf1d421113
vorw <sultanalsanie23@gmail.com>
2025-06-18 16:47:18 +0000

    task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+	
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
