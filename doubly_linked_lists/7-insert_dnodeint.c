14e21ed20729850976758b852ced4c654bfac45c
vorw <sultanalsanie23@gmail.com>
2025-06-20 21:45:39 +0000

    task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node;
 	unsigned int i = 0;
 	dlistint_t *tmp = *h;
-	new_node = malloc(sizeof(dlistint_t));
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
@@ -32,6 +31,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
+
+	new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
 		return (NULL);
