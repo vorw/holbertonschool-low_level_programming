2782fdc61ea4f65cc6fe452e48f6043586a390b5
vorw <sultanalsanie23@gmail.com>
2025-06-22 01:21:42 +0000

    Task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,36 +3,37 @@
 
 int main(int argc, char *argv[])
 {
-	int f_from, f_to, r, w;
+	int f_from, f_to;
+	ssize_t r, w;
 	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(1, "Usage: cp file_from file_to\n");
+		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
 
 	f_from = open(argv[1], O_RDONLY);
 	if (f_from == -1)
 	{
-		dprintf(1, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
 	f_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (f_to == -1)
 	{
-		dprintf(1, "Error: Can't write to %s\n", argv[2]);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		close(f_from);
 		exit(99);
 	}
 
-	while ((r = read(f_from, buf, 1024)) > 0)
+	while ((r = read(f_from, buf, sizeof(buf))) > 0)
 	{
 		w = write(f_to, buf, r);
 		if (w != r)
 		{
-			dprintf(1, "Error: Can't write to %s\n", argv[2]);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			close(f_from);
 			close(f_to);
 			exit(99);
@@ -41,7 +42,7 @@ int main(int argc, char *argv[])
 
 	if (r == -1)
 	{
-		dprintf(1, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		close(f_from);
 		close(f_to);
 		exit(98);
@@ -49,13 +50,13 @@ int main(int argc, char *argv[])
 
 	if (close(f_from) == -1)
 	{
-		dprintf(1, "Error: Can't close fd %d\n", f_from);
+		dprintf(2, "Error: Can't close fd %d\n", f_from);
 		exit(100);
 	}
 
 	if (close(f_to) == -1)
 	{
-		dprintf(1, "Error: Can't close fd %d\n", f_to);
+		dprintf(2, "Error: Can't close fd %d\n", f_to);
 		exit(100);
 	}
 
