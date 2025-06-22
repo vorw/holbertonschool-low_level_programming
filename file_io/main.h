d4d2e27f2d3980e30143676633633f6de627ed03
vorw <sultanalsanie23@gmail.com>
2025-06-22 01:48:05 +0000

    task 3

--- file_io/main.h
+++ file_io/main.h
@@ -1,12 +1,11 @@
 #ifndef _FILE_IO_
 #define _FILE_IO_
 
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
